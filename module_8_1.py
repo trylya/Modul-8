7b9b3ac21fa90da10670d521ae4d8a74cb1c8f10
Лена <trylya@mail.ru>
2024-11-16 23:39:53 +0300

    Try и Except

---
+++ module_8_1.py
@@ -0,0 +1,14 @@
+def add_everything_up(a, b):
+    try:
+        sum = a + b
+    except TypeError:
+        sum = str(a) + str(b)
+    return sum
+
+if __name__ == '__main__':
+
+ print(add_everything_up(123.456, 'строка'))
+ print(add_everything_up('яблоко', 4215))
+ print(add_everything_up(123.456, 7))
+
+
